d8b0c01f27a624f770be72bff73c7c28e5d57410
GeneraLYazeeD <ggeneralyazeedd@gmail.com>
2024-06-11 11:05:51 +0300

    My first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'menu-food-project';
+
+  total=0;
+  qty=0;
+
+  title = 'menu';
+
+  foodmenu=[
+    {id:1,name:"دجاج",price:35,description:"دجاج فروج",img:"assets/images/chicken.jpg"},
+    {id:2,name:"دجاج",price:25,description:"دجاج بروست",img:"assets/images/chicken1.jpg"},
+    {id:3,name:"معصوب",price:15,description:"معصوب قشطة",img:"assets/images/masoub.png"},
+    {id:4,name:"جمبري",price:39,description:"وجبة الجمبري",img:"assets/images/shrimp.jpg"}
+  ]
+
+  sum( event: any, price: number ){
+    const isChecked: boolean=event.target.checked;
+    if(isChecked){
+      this.total=this.total+price
+      this.qty+=1;
+    }
+    else{
+      this.total=this.total-price
+      this.qty-=1;
+    }
+  
+  }
+
+  menuName:string="";
+
+  findMenu(menu:any[]){
+    return menu.filter(item=> item.name.toLowerCase().includes( this.menuName.toLocaleLowerCase()));
+  }
 }
